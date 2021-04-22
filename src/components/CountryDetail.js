de82f9e7d04b558bd2fdf3932879f9503a7ab52a
MiiaNyy <mi.nyyssonen@gmail.com>
2021-04-22 16:03:59 +0300

    function done that fetches countrys name when given country code. Used in naming border countries.

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { CardTitle, Container, Flex } from "../styledComponents/styles";
 import { Link } from "react-router-dom";
 
+
+async function fetchBorderCountry(countryCode) {
+    const response = await fetch(`https://restcountries.eu/rest/v2/alpha?codes=${ countryCode }`);
+    if ( response.status !== 200 ) {
+        throw new Error('cannot fetch border country data');
+    }
+    return await response.json();
+}
+
 function CountryDetail(props) {
+    const [borderCountries, setBorderCountries] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     //const country = props.location.state;
     const country = props.obj;
+
+
+    useEffect(()=>{
+        console.log(country.borders)
+        let borders = country.borders;
+        if(borders.length === 0) {
+            setBorderCountries(['No border countries'])
+            setLoading(true);
+        } else {
+            for (let i = 0; i < borders.length; i++) {
+                fetchBorderCountry(borders[i])
+                    .then((response)=>{
+                        console.log('border countris name is ' + response[0].name);
+                        setBorderCountries((prev) => {
+                            return [...prev, response[0].name]
+                        })
+                        setLoading(true);
+                    })
+            }
+        }
+
+    }, [])
+
     return (
         <>
-            <div className="container">
+            <div className="container btn__cont">
                 <Link to="/" className="link detail__link">
                     <i className="fas fa-arrow-left"/>
                     &nbsp;&nbsp; Go Back
@@ -21,18 +57,62 @@ function CountryDetail(props) {
                     <img src={ country.flags } alt={ `${ country.name } flag` } width="600" height="400"/>
                     <div className="detail__text">
                         <h2>{ country.name }</h2>
-                        <p><strong>Capital</strong>: { country.capital }</p>
-                        <p><strong>Region</strong> : { country.region }</p>
-                        <p><strong>Population</strong> : { country.population }</p>
-                        <p><strong>Capital</strong>: { country.capital }</p>
-                        <p><strong>Region</strong> : { country.region }</p>
-                        <p><strong>Population</strong> : { country.population }</p>
+                        <PrimaryDetails borderCountries={ borderCountries } country={ country }
+                                        loading={ loading }/>
                     </div>
                 </div>
             </div>
         </>
 
     );
+}
+
+function PrimaryDetails(props) {
+
+    const country = props.country;
+    if ( props.loading ) {
+        console.log('border countries are ' + props.borderCountries);
+        return (
+            <div className="detail__primary">
+                <ul>
+                    <li><strong>Native name</strong>: { country.nativeName }</li>
+                    <li><strong>Capital</strong>: { country.capital }</li>
+                    <li><strong>Population</strong> : { country.population }</li>
+                </ul>
+
+                <ul>
+                    <li><strong>Region</strong> : { country.region }</li>
+                    <li><strong>Subregion</strong> : { country.subregion }</li>
+                </ul>
+
+                <ul>
+                    <li><strong>Languages</strong>:
+                        <Sublist arr={ country.languages }/>
+                    </li>
+                    <li><strong>Borders</strong>:
+                        <Sublist arr={ props.borderCountries }/>
+                    </li>
+                    <li>
+                        <strong>Currency</strong> : { country.currencies.name }, { country.currencies.symbol }
+                    </li>
+                </ul>
+
+            </div>
+        )
+    } else {
+        return <><p>Loading not finished</p></>
+    }
+}
+
+function Sublist(props) {
+    return (
+        <ul>
+            {
+                props.arr.map((item)=><li key={ uuidv4() }>{ item }</li>)
+            }
+        </ul>
+
+    )
 }
 
 export default CountryDetail;
--- src/components/helperFunctions/createCountryObj.js
+++ src/components/helperFunctions/createCountryObj.js
@@ -1,4 +1,5 @@
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+
 
 function createCountryObj(obj) {
     return {
@@ -11,7 +12,7 @@ function createCountryObj(obj) {
             symbol: obj.currencies[0].symbol,
         },
         languages: getLanguages(obj.languages),
-        borders: getBorderCountriesNames(obj.borders),
+        borders: obj.borders,
         region: obj.region,
         subregion: obj.subregion,
         flags: obj.flag,
@@ -24,32 +25,7 @@ function numberWithSpaces(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
-function getBorderCountriesNames(arr) {
-    let borderCountries = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        let countryCode = arr[i];
-        fetchBorderCountry(countryCode)
-            .then((response)=>{
-                borderCountries.push(response.name);
-            })
-    }
-    return borderCountries;
-}
-
-async function fetchBorderCountry(countryCode) {
-    const response = await fetch("https://restcountries-v1.p.rapidapi.com/alpha/" + countryCode, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "7a94a0dbcfmsh47721c473016170p131753jsn825aeddb6e44",
-            "x-rapidapi-host": "restcountries-v1.p.rapidapi.com"
-        }
-    });
-    if ( response.status !== 200 ) {
-        throw new Error('cannot fetch border country data');
-    }
-    return await response.json();
-}
 
 function getLanguages(arr) {
     let languages = [];
