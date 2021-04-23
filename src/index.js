e39dfc2354b9336788933046aa29d4401d5d7de9
MiiaNyy <mi.nyyssonen@gmail.com>
2021-04-23 15:35:47 +0300

    added gh-pages package and changed browserrouter to hashrouter so page works in gh pages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './components/App';
+import { HashRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-    <App/>,
+    <React.StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </React.StrictMode>,
     document.getElementById('root')
 );
 
